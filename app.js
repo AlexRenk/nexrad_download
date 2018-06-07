86eaf842b5ae0fb4f4ec73239a268cbd46d4f087
Alexander <qb.alex144@gmail.com>
2018-06-07 18:42:15 +0300

    Upload to rabbitMQ

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const AWS = require('aws-sdk');
 const config = require('config');
-const http = require('http');
-const fs = require('fs');
 const path = require('path');
+const amqp = require('amqplib/callback_api');
+const timestamp = require('unix-timestamp');
 
 AWS.config.update({region: config.get("aws.region")});
 process.env.AWS_ACCESS_KEY_ID = config.get("aws.credentials.id");
@@ -22,8 +22,40 @@ const params = {
     WaitTimeSeconds: 20
 };
 let processingData = false;
+let channel;
+let upload_data = {
+    "id": "",
+    "timestamp": 0,
+    "abbreviation": "",
+    "image_url": "",
+    "image": {},
+    "storage_path": "",
+    "storage_png_path": "",
+    "added": 0
+};
+let year;
+let month;
+let day;
+let hour;
+let min;
+let sec;
 
-setInterval(checking, 1000);
+setInterval(checking, 5000);
+
+amqp.connect(config.get("amqp_url"), function (err, connect) {
+    if (err != null) {
+        console.error(err);
+        process.exit(1);
+    }
+    connect.createChannel(function (err, ch) {
+        if (err != null) {
+            console.error(err);
+            process.exit(1);
+        }
+        ch.assertQueue(config.get("queue_name"));
+        channel = ch;
+    });
+});
 
 function main() {
     sqs.receiveMessage(params, function (err, data) {
@@ -57,10 +89,18 @@ function main() {
                 let link_key = temp.Records[0].s3.object.key;
                 link = config.get("aws.link") + link_key;
 
-                let file = fs.createWriteStream(config.get("data_path") + path.basename(link_key));
-                let request = http.get(link, function (response) {
-                    response.pipe(file);
-                });
+                upload_data.id = upload_data.abbreviation = path.basename(link_key).substr(0, 4);
+                upload_data.image_url = link;
+                year = parseInt(path.basename(link_key).substr(4, 4), 10);
+                month = parseInt(path.basename(link_key).substr(8, 2), 10);
+                day = parseInt(path.basename(link_key).substr(10, 2), 10);
+                hour = parseInt(path.basename(link_key).substr(13, 2), 10);
+                min = parseInt(path.basename(link_key).substr(15, 2), 10);
+                sec = parseInt(path.basename(link_key).substr(17, 2), 10);
+                upload_data.timestamp = timestamp.fromDate(new Date(year, month, day, hour, min, sec));
+                upload_data.added = Math.floor(timestamp.now());
+                channel.sendToQueue(config.get("queue_name"), new Buffer(JSON.stringify(upload_data)));
+                console.log(upload_data.image_url);
             });
             sqs.deleteMessageBatch(deleteParams, function (err, data) {
                 if (err) console.log(err, err.stack);
@@ -72,7 +112,7 @@ function main() {
 }
 
 function checking() {
-    if (processingData === false) {
+    if (processingData === false && typeof channel !== 'undefined') {
         processingData = true;
         main();
     }
